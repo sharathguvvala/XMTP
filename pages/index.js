eb992bb1decce1269253c53af33053f461246de5
sharathguvvala <guvvalasharath@gmail.com>
2022-07-13 23:51:10 +0530

    xmtp

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useSigner, useProvider } from "wagmi";
+import { Client } from "@xmtp/xmtp-js";
 
 export default function Home() {
+  const getSignerOrProvider = async (needSigner = false) => {
+    try {
+      const provider = useProvider();
+      if ((needSigner = true)) {
+        const { data: signer } = useSigner();
+        return signer;
+      }
+      return provider;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendMessage = async () => {
+    try {
+      const signer = await getSignerOrProvider(true);
+      const xmtp = await Client.create(signer);
+      const conversation = await xmtp.conversations.newConversation(
+        "0x4a37dF8B8E1a79c364abB7C86716B5a8E435dC80"
+      );
+      const messages = await conversation.messages();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +41,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ConnectButton />
+        <button onClick={sendMessage}>Send Message</button>
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +51,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
